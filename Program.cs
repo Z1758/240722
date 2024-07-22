01e92749e0d4f46062855e755af5196d87d2a83e
Z1758 <timeofwitch@gmail.com>
2024-07-22 15:42:25 +0900

    feat: static 과제 추가
    계산기 기능 과제

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿namespace _240722
 {
-    class Chracter
+   /* class Chracter
     {
         int level;
         int hp;
@@ -33,12 +33,56 @@
         }
 
     }
+   */
+    public class MyCalculator
+    {
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public static double Divide(double a, double b)
+        {
+            if(a == 0 || b== 0)
+            {
+                Console.WriteLine("0으로 나눌수 없습니다.");
+
+                return 0;
+            }
+
+            return a / b;
+        }
+
+        public static double Squared(double a, double b)
+        {
+            double sum = a;
+            for(int i = 0; i < b-1; i++)
+            {
+                sum *= a;
+            }
+
+            return sum;
+        }
+
+    }
 
     internal class Program
     {
         static void Main(string[] args)
         {
-           Chracter chracter = new Chracter();
+           //Chracter chracter = new Chracter();
+
+           Console.WriteLine(MyCalculator.Squared(2, 4));
         }
     }
 }
