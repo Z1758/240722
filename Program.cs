6dd18f37fbc19af95887df5383852eb600514bca
Z1758 <timeofwitch@gmail.com>
2024-07-22 15:28:21 +0900

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _240722
+{
+    class Chracter
+    {
+        int level;
+        int hp;
+        int moveSpeed;
+        int attack;
+
+        public void FowardMove()
+        {
+        }
+        public void BackMove()
+        {
+
+        }
+
+        public void TurnLeft()
+        {
+
+        }
+        public void TurnRight()
+        {
+
+        }
+        public void Attack()
+        {
+
+        }
+        public void Hit()
+        {
+
+        }
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Chracter chracter = new Chracter();
+        }
+    }
+}
